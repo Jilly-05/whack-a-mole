e63e10538bbf82f037bb5e4f86ce1515d9b7f1f9
Jilly <84245280+Jilly-05@users.noreply.github.com>
2022-05-24 23:27:41 +0900

    “cancleAnimationFrame”

--- pages/whack-a-mole.js
+++ pages/whack-a-mole.js
@@ -1,31 +1,51 @@
 import { useEffect, useState } from "react";
 
 export default function Mole(){
-    const onClick = ()=>{
-        console.log("clicked");
+    let timer=0;
+    let ssyang;
+    const[time, setTime] = useState(10);
+    // count down
+    const main=()=>{
+        if(time==0){
+            console.log("stop");
+            cancelAnimationFrame(ssyang);
+            setTime(5);
+        } else {
+            ssyang = requestAnimationFrame(main);
+            timer= timer+1;
+            if(timer%60==0){
+                time = time-1;
+                setTime(time);
+                console.log(timer);
+            }
+        }
+        
     }
-    const[time, setTime] = useState(0);
-    useEffect(()=>{
-        // const count = setInterval(()=>{
-        //     time=time+1;
-        //     setTime(parseInt(time))
-        //     clearInterval(count);
-        // },1000);
-    },[time])
+    // 두더지 등장
     const [rand,setRandom] = useState(0);
     useEffect(()=>{
         setRandom(Math.floor(Math.random()*6)+1);
     },[time])
-    console.log(rand);
+    // 두더지 잡기
+    const onClick = (e)=>{
+        if(e.target.classList[1] == "mole"){
+            console.log("got the mole");
+            e.target.style.cursor = "/hammer copy.png";
+            console.dir(e.target);
+        } else {
+            console.log("none");
+        }
+    }
     return (
         <main className="main">
-            <p>{time}</p>
+            <p onClick={main}>start</p>
             {/* 배경 이미지 */}
             <div className="background">
+                <p>{time}</p>
                 {/* 두더지 첫번째 줄 */}
                 <div className="line">
                     <div className="hole-mole-set" onClick={onClick}>
-                        {rand === 1 ? <img className="mole" src="/mole.png" /> : <img className="hole" src="/hole.png" />}
+                        {rand === 1 ? <img className="mole" src="/mole.png" style={{animationDuration:"3s",animationName:"slidein",}}/> : <img className="hole" src="/hole.png" />}
                     </div>
                     <div className="hole-mole-set" onClick={onClick}>
                         {rand === 2 ? <img className="mole" src="/mole.png" /> : <img className="hole" src="/hole.png" />}
@@ -75,7 +95,10 @@ export default function Mole(){
                 }
 
                 p{
-                    display:none;
+                    position:reletive;
+                }
+                .mole {
+                    transition: ease-in-out 2s;
                 }
           `}</style>
         </main>
